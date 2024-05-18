edf1f90357d0f7047133971ba7ac8bfe27aa42e4
MD RASEL AHMED <devsniper71@gmail.com>
2024-05-18 20:02:14 +0600

    chore: Add .gitignore file and implement OOP classes for book borrowing system

---
+++ string.php
@@ -0,0 +1,20 @@
+<?php
+$strings = ["Hello", "World", "PHP", "Programming"];
+
+function countVowels($str) {
+    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    $count = 0;
+    foreach (str_split(strtolower($str)) as $char) {
+        if (in_array($char, $vowels)) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
+foreach ($strings as $string) {
+    $vowelCount = countVowels($string);
+    $reversedString = strrev($string);
+    echo "Original String: $string, Vowel Count: $vowelCount, Reversed String: $reversedString\n";
+}
+?>
